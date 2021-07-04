e4f313130fb4e95ef1c5d96e56b550be07a4a79a
ljmcadory <ljmcadory@gmail.com>
2021-07-04 00:41:24 -0400

    scss

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import 'bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
+
+
 createApp(App).use(store).use(router).mount("#app");
